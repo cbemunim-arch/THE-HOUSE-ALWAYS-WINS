1938b797a93c00893059b3e51c80b2921664e4ae
cbemunim-arch <cb.emunim@gmail.com>
2026-06-09 17:02:35 +0300

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'house-always-wins-v1';
+const ASSETS = [
+  './',
+  './index.html' // Assumes you renamed your main HTML file to index.html
+];
+
+// Install the service worker and cache assets
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Activate and clean up old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Required fetch listener to allow installation
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
